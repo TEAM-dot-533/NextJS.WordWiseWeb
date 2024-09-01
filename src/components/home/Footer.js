4df9288dc0dff09c96d7ebce34e116a7fe1f9da8
sydneyskim <seung2332@gmail.com>
2024-09-01 22:20:52 +0900

    add: 메인페이지 기본 틀 생성

---
+++ src/components/home/Footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+        <footer className="bg-gray-800 p-4 mt-8">
+            <p className="text-white text-ceonter">© 2024 TEAM.dot. All rights reserved.</p>
+        </footer>
+    );
+}
