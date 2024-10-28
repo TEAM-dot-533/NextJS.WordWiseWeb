23d7188a7f7a2b288f2ff6e07d9288a8a5de02bc
sydneyskim <seung2332@gmail.com>
2024-10-27 21:41:13 -0700

    chore: 카멜표기법 변경해 오류 수정

---
+++ src/app/wordbook/page.js
@@ -0,0 +1,7 @@
+export default function WordbookPage() {
+  return (
+    <h1 className="text-3xl font-bold text-center">
+      <span className="fa-solid fa-house" aria-label="home icon"></span> Client About
+    </h1>
+  );
+}
